cb8f1164a0557ede3b99a14956d6d694eb38ef67
liampgrichardson <80628115+liampgrichardson@users.noreply.github.com>
2022-06-19 13:56:27 +0100

    fixed timeout error exception

--- database_updater/db_updater.py
+++ database_updater/db_updater.py
@@ -2,7 +2,8 @@ from binance.client import Client
 import pandas as pd
 import sqlite3
 import constants
-
+from datetime import datetime, timedelta
+import requests
 
 client = Client(constants.API_KEY, constants.SECRET_KEY)
 
@@ -28,24 +29,27 @@ def klines_to_df(klines):
     return data_f
 
 
+# initialise table with past day of data
 past_day_df = klines_to_df(client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC"))
-
 conn = sqlite3.connect(f"../app_storage/database.db", check_same_thread=False)
 past_day_df.iloc[:-1].to_sql("BTCUSDT", con=conn, index=True, if_exists='replace')
-dataframe = pd.read_sql_query(f"SELECT * FROM BTCUSDT ORDER BY datetime DESC LIMIT 1;", conn)
-print(f"actual last row in table is :\n {dataframe}")
 conn.commit()
 
-last_minute_table = past_day_df.iloc[-2].name
 
 while True:
-    past_5minute_df = klines_to_df(client.get_historical_klines(
-        "BTCUSDT", Client.KLINE_INTERVAL_1MINUTE, "5 minutes ago UTC"))
-    if last_minute_table != f"{past_5minute_df.iloc[-2].name}":
-        past_5minute_df.iloc[[-2]].to_sql("BTCUSDT", con=conn, index=True, if_exists='append')
-        conn.commit()
-        last_minute_table = past_5minute_df.iloc[-2].name
-        print(f"5 mins kline as df is:\n {past_5minute_df}")
-        print(f"updated last row in table with {last_minute_table}")
-        dataframe = pd.read_sql_query(f"SELECT * FROM BTCUSDT ORDER BY datetime DESC LIMIT 1;", conn)
-        print(f"checking, actual last row in table is :\n {dataframe}")
+    last_row_table = pd.read_sql_query(f"SELECT * FROM BTCUSDT ORDER BY datetime DESC LIMIT 1;", conn)
+    now_time = datetime.utcnow()
+    if now_time - datetime.strptime(last_row_table["datetime"].iloc[-1], '%Y-%m-%d %H:%M:%S') < timedelta(minutes=2):
+        continue
+
+    latest_df = pd.DataFrame()
+
+    try:
+        latest_df = klines_to_df(client.get_historical_klines(
+            "BTCUSDT", Client.KLINE_INTERVAL_1MINUTE, last_row_table["datetime"].iloc[-1]))
+    except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as e:
+        print("error", e, "occurred")
+        continue
+
+    latest_df.iloc[1:-1].to_sql("BTCUSDT", con=conn, index=True, if_exists='append')
+    conn.commit()
